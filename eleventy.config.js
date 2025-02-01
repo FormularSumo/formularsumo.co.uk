deb1e7270c4f210ff222b05126de7cc3dfaf3f7f
FormularSumo <chewy.h@live.co.uk>
2025-02-01 16:44:58 +0100

    Localise URLs, remove testPage from navbar
    URLs are now localised rather than always being in English, and language switcher correctly switches between these URLs use the new localiseURL filter. If an article does not exist in one language it will instead redirect to blog page.
    
    testPage has been removed from navbar as it's currently unfinished and is not localised so would not exist when trying to load in another language.

--- eleventy.config.js
+++ eleventy.config.js
@@ -15,5 +15,43 @@ export default function (eleventyConfig) {
 			lang = "en-gb";
 		}
 		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL, {locale: lang});
-	})
+	});
+
+	eleventyConfig.addFilter("localiseURL", (URL, lang) => {
+		let newURL = "";
+		if (lang == "es") {
+			if (URL.includes("articles")) {
+				URL = URL.split('/').slice(-2,-1)
+				if (URL == "pronouns" ) {
+					newURL = "pronombres";
+				}
+
+				if (newURL != "") {
+					newURL = "/es/articulos/" + newURL;
+				} else {
+					newURL = "/es/blog/"; //If an article is not translated, redirect to blog page
+				}
+			} else { //This currently works because all pages have the same name, will need updating when new pages are added
+				newURL = "/es" + URL
+			}
+		} else if (lang == "en" ) {
+			if (URL.includes("articulos")) {
+				URL = URL.split('/').slice(-2,-1)
+				if (URL == "pronombres" ) {
+					newURL = "pronouns";
+				}
+
+				if (newURL != "") {
+					newURL = "/articles/" + newURL;
+				} else {
+					newURL = "/blog/";
+				}
+			} else {
+				newURL = "/" + URL.split('/').slice(2,-1)
+			}
+		} else { //This is used so default-language English pages ("/") can be passed through without change
+			newURL = URL
+		}
+		return newURL;
+	});
 };
