c5c3083e4690786455b503c48d25776229825104
FormularSumo <chewy.h@live.co.uk>
2026-02-27 18:37:03 +0000

    Software fix spacing between entries

--- shortcodes/details.js
+++ shortcodes/details.js
@@ -1,12 +1,14 @@
 export default (content, summary, platforms) => {
     if (platforms) {
         return `<details>
-            <summary>${summary}</summary>
-            ${content}
-            Platforms: ${platforms}</details>`
+        <summary>${summary}</summary>
+        ${content}
+        Platforms: ${platforms}
+        </details>`
     } else {
         return `<details>
         <summary>${summary}</summary>
-        ${content}</details>`
+        ${content}
+        </details>`
     }
 };
